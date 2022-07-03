9a753510cd819db6a5e0b023de45bbf4e46a0e65
jyoonsong <jaeyoons@mit.edu>
2022-07-03 21:14:54 +0900

    design page done

---
+++ src/data/designProjects.js
@@ -0,0 +1,30 @@
+export const designProjects = [
+    {
+        id: "music",
+        title: "Magic Spells All Muggles are Jealous of",
+        image: "projects/music-bullshit.jpg",
+        description: `Twelve variations of magic spells in Harry Potter series<br/>
+        More information is available at <a className="border-link" href="https://jaeyoon.io/dt4c" target="_blank" rel="noreferrer">https://jaeyoon.io/dt4c</a>`
+    },
+    {
+        id: "zatoori",
+        title: "Zatoori: A Mobile App for Workout Management",
+        image: "projects/zatoori.jpg",
+        description: `A mobile application that helps people exercise in free time<br/>
+        More information is available at <a className="border-link" href="https://www.behance.net/gallery/111082311/Zatoori-A-mobile-application-design" target="_blank" rel="noreferrer">this Behance post</a>`
+    },
+    {
+        id: "bigpearl",
+        title: "Landing Page for a Startup",
+        image: "design/landing.png",
+        description: `A landing page design for an influencer marketing startup<br/>
+        Also available at <a className="border-link" href="https://dribbble.com/shots/3971215-Landing-Page-for-Korean-Startup" target="_blank" rel="noreferrer">this Dribbble post</a>`,
+    },
+    {
+        id: "chartmetric",
+        title: "Redesign of the Chartmetric Ranking Page",
+        image: "design/rank.png",
+        description: `Redesigning the charts at the Chartmetric website<br/>
+        Also available at <a className="border-link" href="https://dribbble.com/shots/3927650-Ranking-Chart-with-Tags" target="_blank" rel="noreferrer">this Dribbble post</a>`,
+    },
+]
--- src/pages/Designer.js
+++ src/pages/Designer.js
@@ -1,10 +1,49 @@
 import React from 'react';
+import { designProjects } from '../data/designProjects';
 
 const Designer = () => {
     return (
         <section className="full-height bg-white">
             <div className="row mb-3">
-                <div className="col-12 col-md-9 col-lg-7 pt-5 pb-4">
+                <div className="col-12 col-md-10 col-lg-8 pt-5 pb-4">
+                    <div className="text-black"><strong>Graphic Design</strong></div>
+                    <div className="text-muted">Sketch App, Adobe Illustrator</div>
+                </div>
+                <div className="col-12 col-md-10 col-lg-8 ">
+                    <div className="row mb-2">
+                        <img src={require("../images/design/magicspell.jpeg")} alt="4 illustration of magic spells" />
+                    </div>
+                    <div className="text-right text-muted text-small ">
+                        <b>Magic Spells All Muggles are Jealous of</b><br/>
+                        Twelve variations of magic spells that appear in Harry Potter series.<br/>
+                        More information is available at <a className="border-link" href="https://jaeyoon.io/dt4c" target="_blank" rel="noreferrer">https://jaeyoon.io/dt4c</a>
+                    </div>
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-12 col-md-10 col-lg-8 pt-4 pb-4">
+                    <div className="text-black"><strong>Web & App Design</strong></div>
+                    <div className="text-muted">Figma, Sketch App</div>
+                </div>
+                <div className="col-12 col-md-10 col-lg-8 ">
+                    <div className="row mb-2">
+                        {designProjects.map(proj => 
+                            <div className="col-12 col-md-6" key={proj.id}>
+                                <img src={require(`../images/${proj.image}`)} alt={proj.title} />
+                                <div className="text-right text-muted  text-small mt-1 mb-3">
+                                    <b>{proj.title}</b><br/>
+                                    <span dangerouslySetInnerHTML={{__html: proj.description}}></span>
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                    
+                </div>
+            </div>
+
+            <div className="row mb-3">
+                <div className="col-12 col-md-10 col-lg-8 pt-4 pb-4">
                     <div className="text-black"><strong>Photography & Drawings</strong></div>
                     <div className="text-muted">&copy; Jaeyoon Song 2022</div>
                 </div>
@@ -13,7 +52,7 @@ const Designer = () => {
                         {[...Array(21).keys()].map(index =>
                             <div className="col-12 col-md-4 mb-2" key={index}>
                                 <div className="image-wrapper">
-                                    <img src={require(`../images/drawings/im${index}-min.jpeg`)} />
+                                    <img src={require(`../images/drawings/im${index}-min.jpeg`)} alt="drawing" />
                                 </div>
                             </div>
                         )}
