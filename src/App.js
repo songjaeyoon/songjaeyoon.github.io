ecb744e46bffb61b76a4b45e12d253dab7c0b11d
jyoonsong <jaeyoons@mit.edu>
2024-06-02 05:21:51 -0400

    fix

--- src/App.js
+++ src/App.js
@@ -9,10 +9,39 @@ import { ParallaxProvider } from "react-scroll-parallax";
 import { createBrowserHistory } from "history";
 import Logo from "./modules/Logo";
 import ScrollToTop from "./modules/ScrollToTop";
+import { useEffect } from "react";
 
 const history = createBrowserHistory();
 
 function App() {
+    useEffect(() => {
+        // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+        let vh = window.innerHeight;
+        // Then we set the value in the --vh custom property to the root of the document
+        document
+            .querySelector(".border")
+            .style.setProperty("height", `${vh}px`);
+
+        // We listen to the resize event
+        window.addEventListener("resize", () => {
+            // We execute the same script as before
+            vh = window.innerHeight;
+            document
+                .querySelector(".border")
+                .style.setProperty("height", `${vh}px`);
+        });
+
+        window.addEventListener("scroll", () => {
+            console.log("scroll");
+            if (vh !== window.innerHeight) {
+                vh = window.innerHeight;
+                document
+                    .querySelector(".border")
+                    .style.setProperty("height", `${vh}px`);
+            }
+        });
+    }, []);
+
     return (
         <ParallaxProvider>
             <Router history={history}>
