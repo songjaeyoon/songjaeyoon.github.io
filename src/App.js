a7a44e1fa93d33d908b74585d736162a7c24ec5d
jyoonsong <jaeyoons@mit.edu>
2024-06-02 05:27:51 -0400

    fix...

--- src/App.js
+++ src/App.js
@@ -7,16 +7,16 @@ import Designer from "./pages/Designer";
 import Home from "./pages/Home";
 import { ParallaxProvider } from "react-scroll-parallax";
 import { createBrowserHistory } from "history";
-import Logo from "./modules/Logo";
 import ScrollToTop from "./modules/ScrollToTop";
 import { useEffect } from "react";
+import { isMobile } from "react-device-detect";
 
 const history = createBrowserHistory();
 
 function App() {
     useEffect(() => {
         // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-        let vh = window.innerHeight;
+        let vh = isMobile ? window.outerHeight : window.innerHeight;
         // Then we set the value in the --vh custom property to the root of the document
         document
             .querySelector(".border")
@@ -25,21 +25,25 @@ function App() {
         // We listen to the resize event
         window.addEventListener("resize", () => {
             // We execute the same script as before
-            vh = window.innerHeight;
+            vh = isMobile ? window.outerHeight : window.innerHeight;
             document
                 .querySelector(".border")
                 .style.setProperty("height", `${vh}px`);
         });
 
-        window.addEventListener("scroll", () => {
-            console.log("scroll");
-            if (vh !== window.innerHeight) {
-                vh = window.innerHeight;
-                document
-                    .querySelector(".border")
-                    .style.setProperty("height", `${vh}px`);
-            }
-        });
+        if (isMobile) {
+            window.addEventListener("scroll", () => {
+                console.log(vh);
+                console.log(window.outerHeight);
+                console.log(window.innerHeight);
+                if (vh !== window.outerHeight) {
+                    vh = window.outerHeight;
+                    document
+                        .querySelector(".border")
+                        .style.setProperty("height", `${vh}px`);
+                }
+            });
+        }
     }, []);
 
     return (
