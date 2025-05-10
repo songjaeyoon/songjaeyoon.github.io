f6238c00c1ba2a3724fe8b26d8664a0b6d8f6d87
jyoonsong <jaeyoons@mit.edu>
2025-05-10 16:06:15 -0400

    not the best, but works

--- src/modules/Navigation.js
+++ src/modules/Navigation.js
@@ -1,48 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 
 const Navigation = () => {
-    // let listenerAttached = false;
-
-    // useEffect(() => {
-    //     const trigger = document.querySelector(".menu-toggle");
-    //     const mobileItems = document.querySelectorAll(".mobile-item");
-
-    //     for (let mobileItem of mobileItems) {
-    //         mobileItem.addEventListener("click", (e) => {
-    //             const mobileNav = document.querySelector(".mobile-menu");
-    //             mobileNav.classList.remove("mobile-menu-toggled");
-    //         });
-    //     }
-
-    //     if (!listenerAttached) {
-    //         listenerAttached = true;
-    //         trigger.addEventListener("click", (e) => {
-    //             const mobileNav = document.querySelector(".mobile-menu");
-
-    //             if (mobileNav.classList.contains("mobile-menu-toggled")) {
-    //                 console.log("untoggled");
-    //                 mobileNav.classList.remove("mobile-menu-toggled");
-    //             } else {
-    //                 console.log("toggled");
-    //                 mobileNav.classList.add("mobile-menu-toggled");
-    //             }
-    //         });
-    //     }
-    // }, []);
+    const [toggleActive, setToggleActive] = useState(false);
 
     return (
         <Navbar expand="lg">
             <Container>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <button
+                    aria-controls="basic-navbar-nav"
+                    type="button"
+                    aria-label="Toggle navigation"
+                    className={`navbar-toggler ${
+                        toggleActive ? "" : "collapsed"
+                    }`}
+                    onClick={() => setToggleActive(!toggleActive)}
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
                 <Navbar.Collapse
                     id="basic-navbar-nav"
-                    className="justify-content-end"
+                    className={`justify-content-end ${
+                        toggleActive ? "show" : ""
+                    }`}
                 >
                     <Nav>
                         <Nav.Link
                             as={NavLink}
+                            onClick={() => setToggleActive(false)}
                             className={(navData) =>
                                 navData.isActive ? "active" : ""
                             }
@@ -52,6 +38,7 @@ const Navigation = () => {
                         </Nav.Link>
                         <Nav.Link
                             as={NavLink}
+                            onClick={() => setToggleActive(false)}
                             className={(navData) =>
                                 navData.isActive ? "active" : ""
                             }
@@ -61,6 +48,7 @@ const Navigation = () => {
                         </Nav.Link>
                         <Nav.Link
                             as={NavLink}
+                            onClick={() => setToggleActive(false)}
                             className={(navData) =>
                                 navData.isActive ? "active" : ""
                             }
@@ -70,6 +58,7 @@ const Navigation = () => {
                         </Nav.Link>
 
                         <Nav.Link
+                            onClick={() => setToggleActive(false)}
                             className={(navData) =>
                                 navData.isActive ? "active" : ""
                             }
@@ -81,6 +70,7 @@ const Navigation = () => {
                         <NavDropdown title="Fun" id="basic-nav-dropdown">
                             <NavDropdown.Item
                                 as={NavLink}
+                                onClick={() => setToggleActive(false)}
                                 className={(navData) =>
                                     navData.isActive ? "active" : ""
                                 }
@@ -90,6 +80,7 @@ const Navigation = () => {
                             </NavDropdown.Item>
                             <NavDropdown.Item
                                 as={NavLink}
+                                onClick={() => setToggleActive(false)}
                                 className={(navData) =>
                                     navData.isActive ? "active" : ""
                                 }
